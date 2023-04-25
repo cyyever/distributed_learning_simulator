650d3fc2f325bd0ad711c3d30f94267636689862
cyy <cyyever@outlook.com>
2023-04-25 17:29:38 +0800

    fix script

--- fed_obd_train.sh
+++ fed_obd_train.sh
@@ -1,1 +1,3 @@
-python3 ./simulator.py --config-name fed_obd/cifar10.yaml
+python3 ./simulator.py --config-name large_scale/fed_obd/cifar10.yaml
+python3 ./simulator.py --config-name large_scale/fed_obd/cifar100.yaml
+python3 ./simulator.py --config-name large_scale/fed_obd/imdb.yaml
