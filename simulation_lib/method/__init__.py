91798cdeb18283a96127803c7cbcf80d86f578ed
cyy <cyyever@outlook.com>
2024-02-04 14:05:04 +0800

    sync with upstream

--- simulation_lib/method/__init__.py
+++ simulation_lib/method/__init__.py
@@ -1,9 +1,16 @@
 from .common_import import *  # noqa: F401
 
+try:
+    from .fed_gcn import *  # noqa: F401
+    from .fed_gnn import *  # noqa: F401
+except BaseException:
+    pass
+from .adaptive_graph_avg import *  # noqa: F401
 from .fed_avg import *  # noqa: F401
 from .fed_dropout_avg import *  # noqa: F401
-from .fed_gcn import *  # noqa: F401
-from .fed_gnn import *  # noqa: F401
 from .fed_obd import *  # noqa: F401
 from .fed_paq import *  # noqa: F401
+from .fed_sparse import *  # noqa: F401
+from .qsgd import *  # noqa: F401
 from .shapley_value import *  # noqa: F401
+from .sign_sgd import *  # noqa: F401
