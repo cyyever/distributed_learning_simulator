f4d7bf90a04386960b86a0083362bc4296f6d16d
cyy <cyyever@outlook.com>
2024-02-07 09:09:55 +0800

    Remove invalid methods

--- simulation_lib/method/__init__.py
+++ simulation_lib/method/__init__.py
@@ -5,12 +5,10 @@ try:
     from .fed_gnn import *  # noqa: F401
 except BaseException:
     pass
-from .adaptive_graph_avg import *  # noqa: F401
 from .fed_avg import *  # noqa: F401
 from .fed_dropout_avg import *  # noqa: F401
 from .fed_obd import *  # noqa: F401
 from .fed_paq import *  # noqa: F401
-from .fed_sparse import *  # noqa: F401
 from .qsgd import *  # noqa: F401
 from .shapley_value import *  # noqa: F401
 from .sign_sgd import *  # noqa: F401
