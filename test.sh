891774e7900864da70be4e1d210b84c763150561
cyy <cyyever@outlook.com>
2024-04-27 15:53:41 +0800

    sync with upstream

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ python3 ./simulator.py --config-name fed_gnn/cs.yaml ++fed_gnn.round=1 ++fed_gnn
 # GTG
 python3 ./simulator.py --config-name gtg_sv/mnist.yaml ++gtg_sv.round=1 ++gtg_sv.epoch=1 ++gtg_sv.worker_number=2 ++gtg_sv.debug=False
 # OBD
-python3 ./simulator.py --config-name fed_obd/cifar10.yaml ++fed_obd.round=1 ++fed_obd.epoch=1 ++fed_obd.worker_number=10 ++fed_obd.algorithm_kwargs.random_client_number=10 ++fed_obd.algorithm_kwargs.second_phase_epoch=1 ++fed_obd.debug=False
+python3 ./simulator.py --config-name fed_obd/cifar10.yaml ++fed_obd.round=2 ++fed_obd.epoch=1 ++fed_obd.worker_number=10 ++fed_obd.algorithm_kwargs.random_client_number=10 ++fed_obd.algorithm_kwargs.second_phase_epoch=1 ++fed_obd.debug=False
