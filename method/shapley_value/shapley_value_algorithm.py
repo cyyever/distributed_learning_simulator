dc73c62fd3b96bb8ea9ff2d37bb2a450579af69a
cyy <cyyever@outlook.com>
2024-04-29 12:47:48 +0800

    sync with upstream

--- method/shapley_value/shapley_value_algorithm.py
+++ method/shapley_value/shapley_value_algorithm.py
@@ -1,9 +1,10 @@
-import copy
 import json
 import os
-from typing import Any, Iterable, Type
+from typing import Any, Type
 
 from cyy_naive_lib.log import get_logger
+from cyy_torch_algorithm.shapely_value.shapley_value import \
+    RoundBasedShapleyValue
 from distributed_learning_simulation import (AggregationServer,
                                              FedAVGAlgorithm, ParameterMessage)
 
@@ -16,43 +17,34 @@ class ShapleyValueAlgorithm(FedAVGAlgorithm):
         self._server: AggregationServer = server
         self.accumulate = False
         self.metric_type: str = "accuracy"
-        self.sv_algorithm = None
+        self.__sv_algorithm: None | RoundBasedShapleyValue = None
         self.sv_algorithm_cls = sv_algorithm_cls
-        self.shapley_values: dict = {}
-        self.shapley_values_S: dict = {}
 
     @property
-    def config(self):
-        return self._server.config
+    def sv_algorithm(self) -> RoundBasedShapleyValue:
+        if self.__sv_algorithm is None:
+            assert self._all_worker_data
+            assert self._server.round_index == 1
+            self.__sv_algorithm = self.sv_algorithm_cls(
+                players=sorted(self._all_worker_data.keys()),
+                initial_metric=self._server.performance_stat[
+                    self._server.round_index - 1
+                ][f"test_{self.metric_type}"],
+            )
+            assert isinstance(self.__sv_algorithm, RoundBasedShapleyValue)
+        return self.__sv_algorithm
 
     @property
     def choose_best_subset(self) -> bool:
         return self.config.algorithm_kwargs.get("choose_best_subset", False)
 
-    def _get_players(self) -> Iterable:
-        return sorted(self._all_worker_data.keys())
-
     def aggregate_worker_data(self) -> ParameterMessage:
-        if self.sv_algorithm is None:
-            assert self._server.round_index == 1
-            self.sv_algorithm = self.sv_algorithm_cls(
-                players=self._get_players(),
-                last_round_metric=self._server.performance_stat[
-                    self._server.round_index - 1
-                ][f"test_{self.metric_type}"],
-            )
-        assert self.sv_algorithm is not None
         self.sv_algorithm.set_metric_function(self._get_subset_metric)
         self.sv_algorithm.compute(round_number=self._server.round_index)
-        self.shapley_values[self._server.round_index] = copy.deepcopy(
-            self._convert_shapley_values(self.sv_algorithm.shapley_values)
-        )
-        self.shapley_values_S[self._server.round_index] = self._convert_shapley_values(
-            self.sv_algorithm.shapley_values_S
-        )
         if self.choose_best_subset:
+            assert hasattr(self.sv_algorithm, "shapley_values_S")
             best_subset: set = set(
-                self.shapley_values_S[self._server.round_index].keys()
+                self.sv_algorithm.shapley_values_S[self._server.round_index].keys()
             )
             if best_subset:
                 get_logger().warning("use subset %s", best_subset)
@@ -60,9 +52,6 @@ class ShapleyValueAlgorithm(FedAVGAlgorithm):
                     k: v for k, v in self._all_worker_data.items() if k in best_subset
                 }
         return super().aggregate_worker_data()
-
-    def _convert_shapley_values(self, shapley_values: dict) -> dict:
-        return shapley_values
 
     def _get_subset_metric(self, subset) -> dict:
         assert subset
@@ -77,16 +66,10 @@ class ShapleyValueAlgorithm(FedAVGAlgorithm):
 
     def exit(self) -> None:
         assert self.sv_algorithm is not None
+        self.sv_algorithm.exit()
         with open(
             os.path.join(self.config.save_dir, "shapley_values.json"),
             "wt",
             encoding="utf8",
         ) as f:
-            json.dump(self.shapley_values, f)
-        if self.choose_best_subset:
-            with open(
-                os.path.join(self.config.save_dir, "shapley_values_S.json"),
-                "wt",
-                encoding="utf8",
-            ) as f:
-                json.dump(self.shapley_values_S, f)
+            json.dump(self.sv_algorithm.get_result(), f)
