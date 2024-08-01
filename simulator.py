ba70bdc22993d8d036a46dcf2666cc20c6bcf888
cyy <cyyever@outlook.com>
2024-08-01 10:11:26 +0800

    sync with upstream

--- simulator.py
+++ simulator.py
@@ -1,16 +1,17 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
-
 import hydra
-from distributed_learning_simulation.config import DistributedTrainingConfig
+from distributed_learning_simulation.config import (DistributedTrainingConfig,
+                                                    import_dependencies)
 from distributed_learning_simulation.config import load_config as __load_config
 from distributed_learning_simulation.training import train
 
+sys.path.insert(0, os.path.abspath("."))
 import method  # noqa: F401
 
 global_config: DistributedTrainingConfig = DistributedTrainingConfig()
+import_dependencies()
 
 
 @hydra.main(config_path="./conf", version_base=None)
