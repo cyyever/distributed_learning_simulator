4d00963d84ee7e9347c3ce3184ee7e1da2ee8965
cyy <cyyever@outlook.com>
2024-08-06 13:02:22 +0800

    Move to other repositary

--- method/fed_gcn/worker.py
+++
@@ -1,7 +0,0 @@
-from distributed_learning_simulation.graph_worker import GraphWorker
-
-
-class FedGCNWorker(GraphWorker):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self._share_feature = True
--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 python3 ./simulator.py --config-name fed_avg/mnist.yaml ++fed_avg.round=1 ++fed_avg.epoch=1 ++fed_avg.worker_number=2 ++fed_avg.debug=True
 # NLP
 python3 ./simulator.py --config-name fed_avg/imdb.yaml ++fed_avg.round=1 ++fed_avg.epoch=1 ++fed_avg.worker_number=2 ++fed_avg.debug=True
-# Graph
-python3 ./simulator.py --config-name fed_gnn/cs.yaml ++fed_gnn.round=1 ++fed_gnn.epoch=1 ++fed_gnn.worker_number=2 ++fed_gnn.debug=False
 # GTG
 python3 ./simulator.py --config-name gtg_sv/mnist.yaml ++gtg_sv.round=1 ++gtg_sv.epoch=1 ++gtg_sv.worker_number=2 ++gtg_sv.debug=False
 # OBD
