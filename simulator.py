df9ea93753b342813c04362e956efbd5992e68dc
cyy <cyyever@outlook.com>
2025-02-25 13:41:15 +0800

    Fix code

--- simulator.py
+++ simulator.py
@@ -5,7 +5,6 @@ from contextlib import redirect_stdout
 from cyy_naive_lib.log import (
     StreamToLogger,
     replace_default_logger,
-    replace_logger,
 )
 from distributed_learning_simulation import import_dependencies, load_config, train
 
@@ -14,14 +13,6 @@ import method  # noqa: F401
 
 import_dependencies()
 if __name__ == "__main__":
-    # disable hydra output dir
-    for option in [
-        "hydra.run.dir=.",
-        "hydra.output_subdir=null",
-        "hydra/job_logging=disabled",
-        "hydra/hydra_logging=disabled",
-    ]:
-        sys.argv.append(option)
     with redirect_stdout(StreamToLogger()):
         replace_default_logger()
         config_path = os.path.join(os.path.dirname(__file__), "conf")
