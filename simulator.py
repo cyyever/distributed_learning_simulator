13e4178fa0011e3b00c21f75ac43408cef3b2644
cyy <cyyever@outlook.com>
2025-02-27 10:02:58 +0800

    Add code

--- simulator.py
+++ simulator.py
@@ -1,11 +1,7 @@
 import os
 import sys
-from contextlib import redirect_stdout
 
-from cyy_naive_lib.log import (
-    StreamToLogger,
-    replace_default_logger,
-)
+from cyy_naive_lib.log import redirect_stdout_to_logger
 from distributed_learning_simulation import import_dependencies, load_config, train
 
 sys.path.insert(0, os.path.abspath("."))
@@ -13,8 +9,7 @@ import method  # noqa: F401
 
 import_dependencies()
 if __name__ == "__main__":
-    with redirect_stdout(StreamToLogger()):
-        replace_default_logger()
+    with redirect_stdout_to_logger():
         config_path = os.path.join(os.path.dirname(__file__), "conf")
         config = load_config(
             config_path=config_path,
